e1501f16ccc9cd7abcea4a2c5222a6b8be96484b
Anvay75 <anvaz2880@gmail.com>
2024-11-06 16:17:19 +0100

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,19 @@
+import turtle as t 
+t.bgcolor("blue")
+t.penup()
+t.goto(326,0)
+t.pendown()
+t.circle(50)
+t.penup()
+t.goto(200,0)
+t.pendown()
+for i in range (3):
+    t.forward(50)
+    t.left(120)
+    
+    
+       
+    
+t.mainloop()
+
+
